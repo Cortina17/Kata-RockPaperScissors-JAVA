2e664292ed7c7f826d4025850e79be2aab5a2266
Cortina17 <diego.cortinagarcia@gmail.com>
2022-03-12 20:04:53 +0100

    Game class and tests done

--- src/main/java/com/factoriaf5/rps/application/Game.java
+++ src/main/java/com/factoriaf5/rps/application/Game.java
@@ -3,9 +3,24 @@ package com.factoriaf5.rps.application;
 public class Game {
     
     public String play(String playerOne, String playerTwo){
+
+        String winner = this.checkWinner(playerOne, playerTwo);
+
+        return winner;
+    }
+    
+    public String checkWinner(String objectOne, String objectTwo){
         
-        if(playerOne == "Rock" && playerTwo == "Scissors" || playerOne == "Scissors" && playerTwo == "Rock"){
+        if(objectOne == "Rock" && objectTwo == "Scissors" || objectOne == "Scissors" && objectTwo == "Rock"){
             return "Rock wins";
+        }
+
+        if(objectOne == "Scissors" && objectTwo == "Paper" || objectOne == "Paper" && objectTwo == "Scissors"){
+            return "Scissors wins";
+        }
+        
+        if(objectOne == "Paper" && objectTwo == "Rock" || objectOne == "Rock" && objectTwo == "Paper"){
+            return "Paper wins";
         }
 
         return "Draw";
--- src/test/java/com/factoriaf5/rps/models/GameTest.java
+++ src/test/java/com/factoriaf5/rps/models/GameTest.java
@@ -29,4 +29,47 @@ public class GameTest {
         assertEquals("Rock wins", result);
     }
 
+    @Test
+    public void scissors_wins_paper(){
+        Game game = new Game();
+        Scissors playerOne = new Scissors();
+        Paper playerTwo = new Paper();
+
+        String result = game.play(playerOne.getName(), playerTwo.getName());
+
+        assertEquals("Scissors wins", result);
+    }
+
+    @Test
+    public void scissors_wins_paper_reverse(){
+        Game game = new Game();
+        Paper playerOne = new Paper();
+        Scissors playerTwo = new Scissors();
+
+        String result = game.play(playerOne.getName(), playerTwo.getName());
+
+        assertEquals("Scissors wins", result);
+    }
+
+    @Test
+    public void paper_wins_rocks(){
+        Game game = new Game();
+        Paper playerOne = new Paper();
+        Rock playerTwo = new Rock();
+
+        String result = game.play(playerOne.getName(), playerTwo.getName());
+
+        assertEquals("Paper wins", result);
+    }
+
+    @Test
+    public void paper_wins_rocks_reverse(){
+        Game game = new Game();
+        Rock playerOne = new Rock();
+        Paper playerTwo = new Paper();
+
+        String result = game.play(playerOne.getName(), playerTwo.getName());
+
+        assertEquals("Paper wins", result);
+    }
 }
