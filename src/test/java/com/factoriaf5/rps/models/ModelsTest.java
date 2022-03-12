150c620155af3db937baba7829e44068dfcc17e7
Cortina17 <diego.cortinagarcia@gmail.com>
2022-03-12 19:23:05 +0100

    tests modified

--- src/test/java/com/factoriaf5/rps/models/ModelsTest.java
+++ src/test/java/com/factoriaf5/rps/models/ModelsTest.java
@@ -5,11 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ModelsTest {
-    
-    @Test
-    public void justASimpleTest() {
-        assertTrue(true);
-    }
 
     @Test
     public void create_rock(){
